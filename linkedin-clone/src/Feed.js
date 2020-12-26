16d4ec7d3315cc1fc78e9df1f00814608d05ebba
kSabchev <kaloyansabchev@gmail.com>
2020-12-26 22:04:45 +0200

    Feed initial commit

---
+++ linkedin-clone/src/Feed.js
@@ -0,0 +1,51 @@
+import React, { useState } from "react";
+import "./Feed.css";
+import CreateIcon from "@material-ui/icons/Create";
+import ImageIcon from "@material-ui/icons/Image";
+import SubscriptionsIcon from "@material-ui/icons/Subscriptions";
+import EventNoteIcon from "@material-ui/icons/EventNote";
+import CalendarViewDayIcon from "@material-ui/icons/CalendarViewDay";
+import InputOption from "./InputOption";
+import Post from "./Post";
+
+function Feed() {
+  const [posts, setPosts] = useState([]);
+
+  return (
+    <div className="feed">
+      <div className="feed__inputContainer">
+        <div className="feed__input">
+          <CreateIcon />
+          <form>
+            <input type="text" />
+            <button type="submit">Send</button>
+          </form>
+        </div>
+        <div className="feed__inputOptions">
+          <InputOption Icon={ImageIcon} title="Photo" color="#70B5F9" />
+          <InputOption Icon={SubscriptionsIcon} title="Video" color="#E7A33E" />
+          <InputOption Icon={EventNoteIcon} title="Event" color="#C0CBDC" />
+          <InputOption
+            Icon={CalendarViewDayIcon}
+            title="Write article"
+            color="#7FC15E"
+          />
+        </div>
+      </div>
+
+      {/* demo post */}
+      <Post
+        name="Kaloyan Sabchev"
+        description="This is a test"
+        message="tomato potato"
+      />
+
+      {/* Posts */}
+      {posts.map((post) => (
+        <Post />
+      ))}
+    </div>
+  );
+}
+
+export default Feed;
