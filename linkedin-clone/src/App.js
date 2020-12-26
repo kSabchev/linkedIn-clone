b1bfb61e5700a9f7c03764575e93ed90c1fb8e0a
kSabchev <kaloyansabchev@gmail.com>
2020-12-26 15:28:38 +0200

    Sidebar

--- linkedin-clone/src/App.js
+++ linkedin-clone/src/App.js
@@ -1,18 +1,19 @@
-import React from 'react';
-import './App.css';
-import Header from './Header';
+import React from "react";
+import "./App.css";
+import Header from "./Header";
+import Sidebar from "./Sidebar";
 
 function App() {
   return (
     <div className="app">
-
       <Header />
-      {/* Header */}
+
       {/* App body */}
-        {/* Sidebar */}
+      <div className="app__body">
+        <Sidebar />
         {/* Feed */}
         {/* Widgets */}
-      
+      </div>
     </div>
   );
 }
--- linkedin-clone/src/HeaderOption.js
+++ linkedin-clone/src/HeaderOption.js
@@ -1,17 +1,15 @@
-import React from 'react'
-import "./HeaderOption.css"
-import {Avatar } from "@material-ui/core";
+import React from "react";
+import "./HeaderOption.css";
+import { Avatar } from "@material-ui/core";
 
-function HeaderOption({ avatar, Icon, title}) {
-    return (
-        <div className="headerOption">
-                {Icon && <Icon className="headerOptions__icon" />}
-                {avatar && (
-                    <Avatar className="headerOption__icon" src={avatar} />
-                )}
-                <h3 className="headerOptions__title">{title}</h3>
-        </div>
-    )
+function HeaderOption({ avatar, Icon, title }) {
+  return (
+    <div className="headerOption">
+      {Icon && <Icon className="headerOptions__icon" />}
+      {avatar && <Avatar className="headerOption__icon" src={avatar} />}
+      <h3 className="headerOptions__title">{title}</h3>
+    </div>
+  );
 }
 
-export default HeaderOption
+export default HeaderOption;
